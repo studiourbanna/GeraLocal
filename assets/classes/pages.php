db0faad2ed12ea576955731a4a0db6fdafdeb20a
Camila L. Oliveira (Mi) <suporte@camilaloliveira.com.br>
2026-01-04 18:29:17 -0300

    Remove All Itens to Second Version

--- assets/classes/pages.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Ler todas as páginas referentes às funções e classes
-	include ("meta.php"); 			//inclui o cabeçalho do tema
-	include ("conexao.php"); 		//inclui a conexao ao banco de dados do sistema
-	//include ("links.php"); 		//referentes aos links das páginas
-	//include ("content.php"); 		//referentes aos conteudos
-?>
