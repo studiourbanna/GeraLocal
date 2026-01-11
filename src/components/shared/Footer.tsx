d7565a189465d02fc4ccd8fada522f0cb76cc271
Milla <camila.leite.oliveira@gmail.com>
2026-01-10 21:41:05 -0300

    Corrigindo ano com Script

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,11 +3,11 @@ import { useTheme } from '@/contexts/ThemeContext';
 
 const Footer: React.FC = () => {
   const { config } = useTheme();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-gray-200 dark:bg-gray-800 p-4 text-center">
-      <p>&copy; 2026 
-          {config?.name || 'Carregando...'}
+      <p>&copy; { currentYear } {config?.name || 'Carregando...'}
         . Todos os direitos reservados. Feito com <a href="https://github.com/studiourbanna/GeraLocal">GeraLocal</a>.</p>
     </footer>
   );
